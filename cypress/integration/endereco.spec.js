fc1328045750b74c71a4af7126355053a14dbd8b
milenaggoes <rm94305@fiap.com.br>
2023-01-12 18:39:26 -0300

    Massa de dados em lista

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -11,6 +12,22 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     it('Deve fazer cadastro de faturamento com sucessso', () => {
         EnderecoPage.editarEnderecoFaturamento('Milena','Goes','ABC','Brasil','Av das Flores','2023','São Paulo','São Paulo','08317150')
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucessso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep
+            )
 
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
