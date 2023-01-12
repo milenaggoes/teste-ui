043877fb2eb40289bd4181698dff6de86d9412c8
milenaggoes <rm94305@fiap.com.br>
2023-01-12 12:29:35 -0300

    Page Objects finalizado

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -10,6 +10,8 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucessso', () => {
-        EnderecoPage.editarEnderecoFaturamento()
+        EnderecoPage.editarEnderecoFaturamento('Milena','Goes','ABC','Brasil','Av das Flores','2023','São Paulo','São Paulo','08317150')
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
